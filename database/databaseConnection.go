b8edfe4ac40bc611a9f5ec3420d92a63246c9193
Leonardo Magnani <69002181+LeonardoMagnani@users.noreply.github.com>
2023-10-25 19:33:55 -0300

    Ajusta dataBaseConnection

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -16,7 +16,7 @@ func DBInstance() *sql.DB {
 	}
 
 	connectionString :=
-		os.Getenv("DB_USER") + os.Getenv("DB_PASSWORD") + ":@" + "(127.0.0.1:3306)/goauth"
+		os.Getenv("DB_USER") + os.Getenv("DB_PASSWORD") + ":@" + "(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DB_NAME")
 
 	db, err := sql.Open("mysql", connectionString)
 
